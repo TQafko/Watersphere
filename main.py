163ef3ba4674afa7046d2933db44e572189c9c76
Tedi.q <tedi.qafko@gmail.com>
2024-04-01 17:39:22 -0400

    fix button

--- main.py
+++ main.py
@@ -71,12 +71,13 @@ def default():
 
 @app.route('/update_data')
 def update_data():
+    contact()
     voltages = read_channels(adc_channels)
     return jsonify(voltages=voltages)
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+# @app.route('/video_feed')
+# def video_feed():
+#     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", threaded=True)
